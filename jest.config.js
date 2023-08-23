78c6fc4205685d694abd7bf80e5621ea7ccd9f1f
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-08-23 11:40:35 +0600

    chore(jest.config.js): create a config for jest

---
+++ jest.config.js
@@ -0,0 +1,51 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: "jsdom",
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!<rootDir>/out/**',
+    '!<rootDir>/.next/**',
+    '!<rootDir>/*.config.js',
+    '!<rootDir>/coverage/**',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy', 
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js', 
+    '^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i': `<rootDir>/__mocks__/fileMock.js`, 
+    '^@/components/(.*)$': '<rootDir>/components/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
+	transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['ts-jest',{
+      tsconfig: 'tsconfig.jest.json'
+    }],
+  },
+	testMatch: [
+		'<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
+		'<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'
+	],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+	moduleFileExtensions: [
+		'ts',
+		'tsx',
+		'js',
+		'jsx',
+		'json',
+		'node'
+	],
+	watchPlugins: [
+		'jest-watch-typeahead/filename',
+		'jest-watch-typeahead/testname'
+	],
+	resetMocks: true,
+};
+
