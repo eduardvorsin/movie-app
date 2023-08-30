c512219ee19e7030b51983c0ef780509cb96ab20
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-08-30 16:27:41 +0600

    refactor(fonts.ts): remove the unused font

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,15 +1,8 @@
-import { Inter, Roboto } from 'next/font/google';
+import { Inter } from 'next/font/google';
 
 export const inter = Inter({
 	subsets: ['latin', 'cyrillic'],
 	weight: ['400', '500', '600', '700'],
 	display: 'swap',
 	variable: '--font-inter',
-});
-
-export const roboto = Roboto({
-	subsets: ['latin', 'cyrillic'],
-	weight: ['400', '500', '700'],
-	display: 'swap',
-	variable: '--font-roboto',
 });
