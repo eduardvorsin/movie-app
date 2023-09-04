944ec40714feaed37a6af4d044ed8b1f97948834
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-09-04 09:45:10 +0600

    refactor(title): make the type with the props exported

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 type HeadingLevel = 1 | 2 | 3 | 4 | 5 | 6;
 type Headings = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
-type Props = {
+export type Props = {
 	children: React.ReactNode,
 	level: HeadingLevel,
 	as?: Headings,
