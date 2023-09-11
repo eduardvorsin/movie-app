68ae210c5cf6b57320851b5945e8d087c8d1f326
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-09-11 18:55:43 +0600

    feat(spacinglist): create a new component

---
+++ src/_docs/blocks/SpacingList.tsx
@@ -0,0 +1,58 @@
+import { Fragment } from 'react';
+
+type SpacingData = { [key: string]: string };
+type Spacing = {
+	name: string,
+	valueInPixels: string,
+	valueInRems: string,
+};
+
+const createSpacingArray = (spacingData: SpacingData): Spacing[] => {
+	return Object.keys(spacingData).map((key) => ({
+		name: `theme.spacing.${key}`,
+		valueInPixels: `${parseFloat(spacingData[key]) * 16}px`,
+		valueInRems: spacingData[key],
+	})).sort((a, b) => parseFloat(a.valueInRems) - parseFloat(b.valueInRems));
+};
+
+type Props = {
+	className?: string,
+	isVertical?: boolean,
+	data: SpacingData,
+};
+
+export default function SpacingList({
+	className,
+	data,
+	isVertical = false,
+}: Props) {
+	const spacingArray = createSpacingArray(data);
+
+	return (
+		<ul
+			className={`${className} !pl-0 !mt-4`}
+		>
+			{spacingArray.map((spacing) => (
+				<li
+					key={spacing.name}
+					className='!mt-0 !mb-3 !last:mb-0 grid gap-3 grid-cols-3 items-center'
+				>
+					<span
+					>
+						{spacing.name}
+					</span>
+					<span>
+						{spacing.valueInRems} / {spacing.valueInPixels}
+					</span>
+					<div
+						className={'bg-green-500'}
+						style={{
+							width: isVertical ? '12rem' : spacing.valueInRems,
+							height: isVertical ? spacing.valueInRems : '2rem',
+						}}
+					/>
+				</li>
+			))}
+		</ul>
+	);
+};
