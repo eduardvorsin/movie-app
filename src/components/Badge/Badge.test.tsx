0c1e8d83af2ce88c5575423b127fb6db1cb5e4b3
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-09-13 19:19:54 +0600

    test(badge.test): write tests for the component

---
+++ src/components/Badge/Badge.test.tsx
@@ -0,0 +1,47 @@
+import { render, screen } from '@testing-library/react';
+import Badge from './Badge';
+
+describe('Badge tests', () => {
+	it('is rendered correctly', () => {
+		render(<Badge>Badge</Badge>);
+
+		expect(screen.getByText<HTMLSpanElement>(/badge/i)).toBeInTheDocument();
+	});
+
+	it('is a basic snapshot', () => {
+		render(<Badge>Badge</Badge>);
+
+		expect(screen.getByText<HTMLSpanElement>(/badge/i)).toMatchSnapshot();
+	});
+
+	it('is a snapshot with the appearance type "default"', () => {
+		render(<Badge appearance='default'>Badge</Badge>);
+
+		expect(screen.getByText<HTMLSpanElement>(/badge/i)).toMatchSnapshot();
+	});
+
+	it('is a snapshot with the appearance type "info"', () => {
+		render(<Badge appearance='info'>Badge</Badge>);
+
+		expect(screen.getByText<HTMLSpanElement>(/badge/i)).toMatchSnapshot();
+	});
+
+	it('is a snapshot with the appearance type "sucsess"', () => {
+		render(<Badge appearance='sucsess'>Badge</Badge>);
+
+		expect(screen.getByText<HTMLSpanElement>(/badge/i)).toMatchSnapshot();
+	});
+
+	it('is a snapshot with the appearance type "warning"', () => {
+		render(<Badge appearance='warning'>Badge</Badge>);
+
+		expect(screen.getByText<HTMLSpanElement>(/badge/i)).toMatchSnapshot();
+	});
+
+	it('is a snapshot with the appearance type "danger"', () => {
+		render(<Badge appearance='danger'>Badge</Badge>);
+
+		expect(screen.getByText<HTMLSpanElement>(/badge/i)).toMatchSnapshot();
+	});
+});
+
