618c8959d7a22d607438d2f1be363d7486959872
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-10-03 09:47:53 +0600

    feat(characteristiclist.tsx): create a new component

---
+++ src/components/CharacteristicList/CharacteristicList.tsx
@@ -0,0 +1,42 @@
+import { createPersonCharacteristicsArray } from 'src/helpers/helpers';
+
+type Props = {
+	className?: string,
+	data: { [key: string]: string | string[] | number | null | boolean | object },
+	testId?: string,
+};
+
+export default function CharacteristicList({
+	className,
+	data,
+}: Props) {
+	const characteristicListClasses = [
+		className,
+	].join(' ');
+
+	const characteristicData = createPersonCharacteristicsArray(data);
+
+	return (
+		<ul
+			className={characteristicListClasses}
+		>
+			{characteristicData.map((characteristic) => (
+				<li
+					key={characteristic.name}
+					className='text-100 flex py-2 break-all'
+				>
+					<span
+						className='text-neutral-700 dark:text-dark-neutral-700 basis-[8rem] xs:basis-[10rem] shrink-0 grow-0 mr-2'
+					>
+						{characteristic.name}:
+					</span>
+					<span
+						className='text-neutral-1000 dark:text-dark-neutral-900 flex-grow'
+					>
+						{characteristic.value}
+					</span>
+				</li>
+			))}
+		</ul>
+	);
+};
