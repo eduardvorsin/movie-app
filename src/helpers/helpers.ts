4df8c3ec15764999033bc813d583f2f34ba18a09
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-10-08 12:57:36 +0600

    feat(helpers.ts): create a new getLocalizedDate function

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -84,3 +84,12 @@ export const getLocalesFromString = (localesString: string): string[] => {
 	return locales;
 };
 
+export const getLocalizedDate = (date: string, lang: string): string => {
+	if (Number.isNaN(Date.parse(date))) {
+		throw new Error('Строка с датой должна быть в формате "YYYY-MM-DDTHH:mm:ss.sssZ"');
+	}
+
+	const dateFormatter = new Intl.DateTimeFormat([lang, fallbackLng]);
+	const timeStamp = Date.parse(date);
+	return dateFormatter.format(new Date(timeStamp));
+};
