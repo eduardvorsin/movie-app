34a68fddff5724d2176e78fde2ca2852e9f40778
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-10-23 09:14:10 +0600

    refactor(search.tsx): make the type with the props exported

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,7 +4,7 @@ import Button from '../Button/Button';
 import InlineMessage from '../InlineMessage/InlineMessage';
 import Loupe from '../../assets/icons/loupe.svg?url';
 
-type Props = {
+export type Props = {
 	value: string,
 	id: string,
 	label: string,
