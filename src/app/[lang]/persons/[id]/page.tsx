0f2e0fb1567a597c3d19424e2dd18a97305fc45b
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-11-17 08:48:17 +0600

    style(persons/[id]/): change the adaptive grid for the page

--- src/app/[lang]/persons/[id]/page.tsx
+++ src/app/[lang]/persons/[id]/page.tsx
@@ -10,6 +10,7 @@ import { Locales } from '@/i18n/settings';
 import { getLocalizedDate } from '@/i18n/utils/getLocalizedDate/getLocalizedDate';
 import { fetchActor } from '@/services/fetchActor/fetchActor';
 import { fetchImageWithPlaceholder } from 'src/helpers/fetchImageWithPlaceholder/fetchImageWithPlaceholder';
+import Container from '@/components/Container/Container';
 
 const characteristicFields = new Set([
 	'known_for_department',
@@ -69,20 +70,73 @@ export default async function Page({ params: { id, lang } }: Props) {
 		});
 
 	return (
-		<div
-			className='max-w-[77rem] mx-auto my-0 py-4 px-4 bg-neutral-0 dark:bg-dark-neutral-250 grid items-start grid-rows-[min-content_min-content] grid-cols-1 sm:grid-cols-[16.25rem_1fr] md:grid-cols-[18.75rem_1fr] lg:grid-cols-[21.375rem_1fr] gap-y-5 md:gap-y-6 lg:gap-y-8 gap-x-8 md:gap-x-10 lg:gap-x-12'
+		<Container
+			className='bg-neutral-0 dark:bg-dark-neutral-250 py-4 sm:py-6 md:py-8 lg:py-10 min-h-[100vh] h-full flex flex-col sm:flex-row items-start'
 		>
-			<Image
-				className='object-cover rounded-2 max-w-[14.375rem] bg-neutral-300 dark:bg-dark-neutral-100 sm:max-w-[16.25rem] md:max-w-[18.75rem] lg:max-w-[21.375rem] justify-self-center'
-				width={342}
-				height={513}
-				src={imageData.img.src}
-				alt={actor.name}
-				sizes='(min-width: 1024px) 342px, (min-width: 768px) 300px, (min-width: 640px) 260px, 230px'
-				placeholder='blur'
-				blurDataURL={imageData.base64}
-				priority
-			/>
+			<div
+				className='basis-full sm:basis-[16.25rem] md:basis-[18.75rem] lg:basis-[21.375rem] mr-0 sm:mr-8 md:mr-10 lg:mr-12 self-center sm:self-start flex flex-col items-center'
+			>
+				<Image
+					className='object-cover rounded-2 max-w-[14.375rem] mb-5 md:mb-6 lg:mb-8 bg-neutral-300 dark:bg-dark-neutral-100 sm:max-w-[16.25rem] md:max-w-[18.75rem] lg:max-w-[21.375rem]'
+					width={342}
+					height={513}
+					src={imageData.img.src}
+					alt={actor.name}
+					sizes='(min-width: 1024px) 342px, (min-width: 768px) 300px, (min-width: 640px) 260px, 230px'
+					placeholder='blur'
+					blurDataURL={imageData.base64}
+					priority
+				/>
+
+				<section>
+					<Title
+						className='sr-only'
+						level={4}
+						as='h2'
+					>
+						Links to social networks
+					</Title>
+					{actor.homepage && (
+						<p className='flex items-center flex-wrap'>
+							<span className='text-dark-neutral-250 dark:text-neutral-300 mr-2'>
+								{t('personalWebsite', { ns: 'personsPage' })}
+							</span>
+							<Link
+								className='flex items-center'
+								href={actor.homepage}
+								target='_blank'
+								isExternal
+							>
+								{actor.homepage}
+								<svg className='w-4 h-4 ml-2 fill-current' viewBox='0 0 32 32'>
+									<use href={'/assets/icons/external-link.svg#external-link'}></use>
+								</svg>
+							</Link>
+						</p>
+					)}
+
+					<div className='flex items-center flex-wrap -ml-[0.375rem]'>
+						{socialNetworks.map((item) => (
+							<div
+								className='px-[0.375rem] pt-3'
+								key={item.name}
+							>
+								<Link
+									className='text-0 flex w-10 h-10 fill-neutral-300 text-dark-neutral-300 hover:fill-neutral-400 hover:text-dark-neutral-250 active:fill-dark-neutral-500 active:text-dark-neutral-200 dark:fill-dark-neutral-400 dark:text-neutral-300 dark:hover:fill-dark-neutral-500 dark:hover:text-neutral-200 dark:active:fill-dark-neutral-600 dark:active:text-neutral-100 transition-colors duration-150'
+									href={item.url}
+									isExternal
+									target='_blank'
+								>
+									<svg viewBox='0 0 32 32'>
+										<use href={socialNetworkIcons[item.name]}></use>
+									</svg>
+									{t('viewProfile', { ns: 'personsPage' })} {item.name}
+								</Link>
+							</div>
+						))}
+					</div>
+				</section>
+			</div>
 
 			<div className='flex flex-col'>
 				<Title
@@ -93,81 +147,41 @@ export default async function Page({ params: { id, lang } }: Props) {
 					{actor.name}
 				</Title>
 
-				<Title
-					className='text-neutral-1000 dark:text-dark-neutral-1000 mb-1 xs:mb-2'
-					level={4}
-					weight={500}
-					as='h2'
-				>
-					{t('aboutPerson', { ns: 'personsPage' })}
-				</Title>
-
-				<CharacteristicList
-					className='max-w-[36.25rem]'
-					data={characteristicData}
-				/>
-			</div>
-
-			<div>
-				{actor.homepage && (
-					<p className='flex items-center flex-wrap'>
-						<span className='text-dark-neutral-250 dark:text-neutral-300 mr-2'>
-							{t('personalWebsite', { ns: 'personsPage' })}
-						</span>
-						<Link
-							className='flex items-center'
-							href={actor.homepage}
-							target='_blank'
-							isExternal
-						>
-							{actor.homepage}
-							<svg className='w-4 h-4 ml-2 fill-current' viewBox='0 0 32 32'>
-								<use href={'/assets/icons/external-link.svg#external-link'}></use>
-							</svg>
-						</Link>
-					</p>
-				)}
-
-				<div className='flex items-center flex-wrap -ml-[0.375rem]'>
-					{socialNetworks.map((item) => (
-						<div
-							className='px-[0.375rem] pt-3'
-							key={item.name}
-						>
-							<Link
-								className='text-0 flex w-10 h-10 fill-neutral-300 text-dark-neutral-300 hover:fill-neutral-400 hover:text-dark-neutral-250 active:fill-dark-neutral-500 active:text-dark-neutral-200 dark:fill-dark-neutral-400 dark:text-neutral-300 dark:hover:fill-dark-neutral-500 dark:hover:text-neutral-200 dark:active:fill-dark-neutral-600 dark:active:text-neutral-100 transition-colors duration-150'
-								href={item.url}
-								isExternal
-								target='_blank'
-							>
-								<svg viewBox='0 0 32 32'>
-									<use href={socialNetworkIcons[item.name]}></use>
-								</svg>
-								{t('viewProfile', { ns: 'personsPage' })} {item.name}
-							</Link>
-						</div>
-					))}
-				</div>
-			</div>
-
-			{biography.length !== 0 && (
-				<div className='col-span-full lg:col-span-1'>
+				<section>
 					<Title
-						className='text-neutral-1000 dark:text-dark-neutral-1000 mb-2'
+						className='text-neutral-1000 dark:text-dark-neutral-1000 mb-1 xs:mb-2'
 						level={4}
 						weight={500}
-						as='h3'
+						as='h2'
 					>
-						{t('biography', { ns: 'personsPage' })}
+						{t('aboutPerson', { ns: 'personsPage' })}
 					</Title>
-					<ExpandableText
-						className='text-neutral-900 dark:text-dark-neutral-900'
-						visibleRowsCount={2}
-					>
-						{biography}
-					</ExpandableText>
-				</div>
-			)}
-		</div>
+
+					<CharacteristicList
+						className='max-w-[36.25rem] mb-4 md:mb-5 lg:mb-6'
+						data={characteristicData}
+					/>
+				</section>
+
+				{biography.length !== 0 && (
+					<section>
+						<Title
+							className='text-neutral-1000 dark:text-dark-neutral-1000 mb-2'
+							level={4}
+							weight={500}
+							as='h2'
+						>
+							{t('biography', { ns: 'personsPage' })}
+						</Title>
+						<ExpandableText
+							className='text-neutral-900 dark:text-dark-neutral-900'
+							visibleRowsCount={2}
+						>
+							{biography}
+						</ExpandableText>
+					</section>
+				)}
+			</div>
+		</Container>
 	);
 }
