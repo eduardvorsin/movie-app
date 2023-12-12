cf834abd132cf4593e3522a730a43acb8954ad8b
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-12-12 12:24:23 +0600

    style(carouselarrow.tsx): reduce the size of the arrows on mobile phones

--- src/components/Carousel/CarouselArrow/CarouselArrow.tsx
+++ src/components/Carousel/CarouselArrow/CarouselArrow.tsx
@@ -17,7 +17,7 @@ export default function CarouselArrow({
 	...props
 }: Props) {
 	const classes = [
-		'p-1 absolute z-100 top-1/2 -translate-y-1/2 bg-neutral-300/80 enabled:hover:bg-neutral-300/[0.85] enabled:active:bg-neutral-300/90 dark:bg-dark-neutral-300/80 dark:enabled:hover:bg-dark-neutral-300/[0.85] dark:enabled:active:bg-dark-neutral-300/90 text-blue-700 enabled:hover:text-blue-800 enabled:active:text-blue-900 dark:text-blue-300 dark:enabled:hover:text-blue-400 dark:enabled:active:text-blue-500 transition-colors transition-opacity duration-150 disabled:opacity-disabled disabled:cursor-not-allowed',
+		'p-2 sm:p-1 absolute z-100 top-1/2 -translate-y-1/2 bg-neutral-300/80 enabled:hover:bg-neutral-300/[0.85] enabled:active:bg-neutral-300/90 dark:bg-dark-neutral-300/80 dark:enabled:hover:bg-dark-neutral-300/[0.85] dark:enabled:active:bg-dark-neutral-300/90 text-blue-700 enabled:hover:text-blue-800 enabled:active:text-blue-900 dark:text-blue-300 dark:enabled:hover:text-blue-400 dark:enabled:active:text-blue-500 transition-colors transition-opacity duration-150 disabled:opacity-disabled disabled:cursor-not-allowed',
 		direction === 'left' ? 'left-[0.625rem]' : 'right-[0.625rem]',
 		className
 	].join(' ');
@@ -32,7 +32,7 @@ export default function CarouselArrow({
 			data-testid={testId}
 			{...props}
 		>
-			<svg className='w-10 h-10' viewBox='0 0 32 32'>
+			<svg className='w-6 h-6 sm:w-10 sm:h-10' viewBox='0 0 32 32'>
 				<use href={`/assets/icons/${iconFilename}`}></use>
 			</svg>
 			<span className='sr-only'>{direction.toUpperCase()} carousel control</span>
