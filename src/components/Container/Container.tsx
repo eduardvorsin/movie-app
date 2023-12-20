14ee08911c89687081320081bc89027c9cd851f8
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-12-20 13:07:46 +0600

    style(container.tsx): remove the extra width prop

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,29 +1,28 @@
 import { ElementType, ReactNode } from 'react';
+import { twMerge } from 'tailwind.config';
 
 type Props = {
 	className?: string,
 	testId?: string,
 	children: ReactNode,
 	component?: ElementType,
-	width?: number,
 };
 
 export default function Container({
 	className,
 	testId,
 	component,
-	width,
 	children,
 }: Props) {
 	const Container = component ?? 'div';
-	const classes = [
-		'mx-auto px-4',
+	const classes = twMerge(
+		'max-w-[77rem] mx-auto px-4',
 		className,
-	].join(' ');
+	);
+
 	return (
 		<Container
 			className={classes}
-			style={{ maxWidth: width ? `${width}px` : '77rem', }}
 			data-testid={testId}
 		>
 			{children}
