5444423e5c7bc9c4adbfe3c99770aab6344fe79c
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-12-25 17:43:29 +0600

    refactor(getlocalizeddate.ts): change the internal implementation of the method

--- src/i18n/utils/getLocalizedDate/getLocalizedDate.ts
+++ src/i18n/utils/getLocalizedDate/getLocalizedDate.ts
@@ -1,12 +1,12 @@
 import { Locales, fallbackLng } from '@/i18n/settings';
 
-export const getLocalizedDate = (date: string, lang: Locales): string => {
+export const getLocalizedDate = (date: string, lang: Locales, options?: Intl.DateTimeFormatOptions): string => {
 	if (!date.length) return '-';
 	if (Number.isNaN(Date.parse(date))) {
 		throw new Error('Строка с датой должна быть в формате "YYYY-MM-DDTHH:mm:ss.sssZ"');
 	}
 
-	const dateFormatter = new Intl.DateTimeFormat([lang, fallbackLng]);
 	const timeStamp = Date.parse(date);
-	return dateFormatter.format(new Date(timeStamp));
+	return new Date(timeStamp).toLocaleDateString([lang, fallbackLng], options);
 };
+
