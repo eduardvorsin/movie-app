00e7783ed7fb79441f460270ceac26548788b2ad
eduardvorsin <eduardvorsin2019@yandex.kz>
2024-01-07 16:51:04 +0600

    feat(loadinglayout.tsx): add basic props to the component

--- src/components/LoadingLayout/LoadingLayout.tsx
+++ src/components/LoadingLayout/LoadingLayout.tsx
@@ -1,8 +1,17 @@
 import Spinner from '@/components/Spinner/Spinner';
+import { GeneralProps } from '@/types/shared';
 
-export default function LoadingLayout() {
+export default function LoadingLayout({
+	className,
+	testId,
+	...props
+}: GeneralProps) {
 	return (
-		<div className='w-full min-h-[100vh] relative'>
+		<div
+			className={`w-full min-h-[100vh] relative ${className}`}
+			data-testid={testId}
+			{...props}
+		>
 			<Spinner
 				className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
 				size='xlarge'
