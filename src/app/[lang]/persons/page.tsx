d085eb64e86e88839ea87330143b5361f4cb39ac
eduardvorsin <eduardvorsin2019@yandex.kz>
2024-01-11 16:43:44 +0600

    feat(persons/page.tsx): create a page with popular actors

---
+++ src/app/[lang]/persons/page.tsx
@@ -0,0 +1,66 @@
+import Breadcrumbs from '@/components/Breadcrumbs/Breadcrumbs';
+import Container from '@/components/Container/Container';
+import ExpandableText from '@/components/ExpandableText/ExpandableText';
+import InfinitePersonFeed from '@/components/InfinitePersonFeed/InfinitePersonFeed';
+import Title from '@/components/Title/Title';
+import { fetchTranslation } from '@/i18n/server';
+import { Locales } from '@/i18n/settings';
+import { fetchPopularPersons } from '@/services/fetchPopularPersons/fetchPopularPersons';
+import { notFound } from 'next/navigation';
+
+type Props = {
+	params: {
+		lang: Locales,
+	}
+};
+
+export default async function Page({ params: { lang } }: Props) {
+	const { t } = await fetchTranslation(lang, 'personsPage');
+	const popularPersons = await fetchPopularPersons(1, { lang });
+
+	if (!popularPersons || popularPersons.results.length === 0) {
+		notFound();
+	}
+
+	return (
+		<main className='mt-[3.75rem]'>
+			<section className='py-3 md:py-5'>
+				<Container>
+					<Breadcrumbs
+						className='px-4 xl:px-0 mb-[1.25rem]'
+						label='breadcrumbs navigation'
+					/>
+
+					<Title
+						className='mb-2 lg:mb-3 text-neutral-1000 dark:text-dark-neutral-900'
+						level={2}
+						as='h1'
+					>
+						{t('mainTitle')}
+					</Title>
+
+					<ExpandableText>
+						{[
+							t('pageDescription_1'),
+							t('pageDescription_2'),
+						]}
+					</ExpandableText>
+				</Container>
+			</section>
+
+			<section className='py-3 md:py-5'>
+				<Container className='flex flex-col'>
+					<Title
+						className='sr-only mb-2 lg:mb-3 text-neutral-1000 dark:text-dark-neutral-900'
+						level={3}
+						as='h2'
+					>
+						{t('personsTitle')}
+					</Title>
+
+					<InfinitePersonFeed initialData={popularPersons} />
+				</Container>
+			</section>
+		</main>
+	);
+}
