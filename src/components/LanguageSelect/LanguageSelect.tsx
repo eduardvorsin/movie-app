a9ba673e1bd509231538252f5f70236831c51074
eduardvorsin <eduardvorsin2019@yandex.kz>
2024-01-18 16:16:40 +0600

    feat(languageselect.tsx): create a new component

---
+++ src/components/LanguageSelect/LanguageSelect.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { useTranslation } from '@/i18n/client';
+import { Locales, fallbackLng } from '@/i18n/settings';
+import { GeneralProps } from '@/types/shared';
+import { useParams, usePathname, useRouter } from 'next/navigation';
+import Select from '../Select/Select';
+import { useId, useState } from 'react';
+
+type LanguageOption = {
+	label: 'RU' | 'EN',
+	value: 'ru' | 'en',
+}
+
+const languageOptions: LanguageOption[] = [
+	{
+		label: 'RU',
+		value: 'ru',
+	},
+	{
+		label: 'EN',
+		value: 'en',
+	},
+]
+
+type Props = GeneralProps;
+
+export default function LanguageSelect({
+	className,
+	testId,
+}: Props) {
+	const id = useId();
+	const currentLang = useParams()?.lang as Locales ?? fallbackLng;
+	const { t, i18n } = useTranslation(currentLang);
+	const [lang, setLang] = useState<Locales>(currentLang);
+	const pathname = usePathname();
+	const router = useRouter();
+
+	const changeHandler = () => {
+		const otherLang = lang === 'ru' ? 'en' : 'ru';
+		setLang(otherLang);
+		i18n.changeLanguage(otherLang);
+		router.replace(pathname.replace(currentLang, otherLang));
+	}
+
+	return (
+		<Select
+			className={className}
+			testId={testId}
+			id={id}
+			labelHidden
+			label={t('languageSelect.label')}
+			name='language select'
+			options={languageOptions}
+			value={lang}
+			closeMenuOnSelect
+			closeMenuOnScroll
+			openMenuOnFocus
+			onChange={changeHandler}
+			minMenuHeight={84}
+			maxMenuHeight={84}
+		/>
+	);
+}
