50c6f4e083daa74edbe5796a4d86d3852ca3c847
eduardvorsin <eduardvorsin2019@yandex.kz>
2024-01-20 09:02:26 +0600

    style(selectoption.tsx): update styles for component states

--- src/components/Select/SelectOption/SelectOption.tsx
+++ src/components/Select/SelectOption/SelectOption.tsx
@@ -26,8 +26,8 @@ export default function SelectOption({
 	const classes = [
 		'outline-none break-words px-[0.625rem] py-[0.375rem] text-100 leading-2 transition-colors duration-150 select-none relative before:w-[0.125rem] before:h-full before:absolute before:left-0 before:top-0 before:transition-colors before:duration-150',
 		(!isFocused && isSelected) || (isFocused && isSelected) ? 'bg-blue-200 text-blue-900 dark:text-blue-300 dark:bg-blue-900' : '',
-		isFocused && !isSelected ? 'bg-neutral-300 dark:bg-dark-neutral-350 before:bg-blue-700 dark:before:bg-blue-400' : '',
-		!isFocused && !isSelected ? 'bg-neutral-0 dark:bg-dark-neutral-250 text-neutral-700 dark:text-dark-neutral-700' : '',
+		isFocused && !isSelected ? 'bg-neutral-300 dark:bg-dark-neutral-350 before:bg-blue-700 dark:before:bg-blue-400 text-neutral-800 dark:text-dark-neutral-800' : '',
+		!isFocused && !isSelected ? 'bg-neutral-0 dark:bg-dark-neutral-250 text-neutral-800 dark:text-dark-neutral-800' : '',
 		className,
 	].join(' ');
 
