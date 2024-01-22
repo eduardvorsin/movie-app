58eab075fb76cb1b1588a627b40ba5829e6d4c5c
eduardvorsin <eduardvorsin2019@yandex.kz>
2024-01-22 18:46:54 +0600

    refactor(createfilmographydata.ts): fix typing errors

--- src/helpers/createFilmographyData/createFilmographyData.ts
+++ src/helpers/createFilmographyData/createFilmographyData.ts
@@ -1,8 +1,13 @@
-import { PersonCredit, PersonCredits } from '@/services/fetchPerson/fetchPerson';
+import { CreditsResponse, PersonCredit } from '@/services/types';
 import { Department } from '@/types/shared';
 
 type Filmography = Record<Lowercase<Department>, PersonCredit[]>;
-export const createFilmographyData = (credits: PersonCredits): Filmography => {
+export const createFilmographyData = (
+	credits: CreditsResponse<
+		PersonCredit,
+		PersonCredit & { department: Department }
+	>
+): Filmography => {
 	const filmography = {} as Filmography;
 	filmography.actors = [...credits.cast];
 
